f63825128ce921f7f0491eaf7a854d0670b01df0
Saya <sajkasajko@gmail.com>
2025-10-03 00:09:18 +0500

    Initial commit

---
+++ cmd/api/main.go
@@ -0,0 +1,11 @@
+package main //Programs start running in package main.
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Hello and welcome!")
+	fmt.Println("The time is", time.Now())
+}
