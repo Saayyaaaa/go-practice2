2621f63a64aabe255e1ec66fb647b52604686567
Saya <sajkasajko@gmail.com>
2025-10-03 01:18:02 +0500

    basics

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,10 +2,31 @@ package main //Programs start running in package main.
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Fprintln(w, "GET Request")
+
+	case http.MethodPost:
+		fmt.Fprintln(w, "POST Request")
+
+	default:
+		fmt.Fprintf(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
-	fmt.Println("Hello and welcome!")
-	fmt.Println("The time is", time.Now())
+
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/user", handleRequest)
+	http.ListenAndServe(":8080", nil)
 }
